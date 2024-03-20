6b4334d8faec1c2444c281b69b367a36fca6379b
Adrian (Zyian) <adrian@zyian.dev>
2024-03-20 13:28:52 -0600

    updates

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,11 @@ use std::ffi::{c_char, c_ulong, c_void, CStr};
 use std::fs::File;
 use std::io::{Read, Write};
 use std::mem::MaybeUninit;
-use std::{fs, ptr};
+use std::{fs, ptr, thread};
 use std::ptr::NonNull;
 use arcdps_imgui::{Context, Ui};
 use arcdps_imgui::sys::{igSetAllocatorFunctions, igSetCurrentContext};
-use nexus_rs::raw_structs::{AddonAPI, AddonDefinition, AddonVersion, EAddonFlags, ELogLevel, LPVOID};
+use nexus_rs::raw_structs::{AddonAPI, AddonDefinition, AddonVersion, EAddonFlags, ELogLevel, ERenderType, LPVOID};
 use walkdir::WalkDir;
 use windows::core::{PCSTR, s};
 use windows::Win32::Foundation::{HINSTANCE};
@@ -47,6 +47,7 @@ unsafe extern "C" fn load(api: *mut AddonAPI) {
 
 
     (api.add_simple_shortcut)(convert_string(SHORTCUT_ID), addon_shortcut);
+    (api.register_render)(ERenderType::OptionsRender, render_options);
 }
 
 pub fn convert_string(s: &str) -> *const c_char {
@@ -54,64 +55,67 @@ pub fn convert_string(s: &str) -> *const c_char {
     a.as_ptr() as *const c_char
 }
 
-pub unsafe fn run_backup() {
-    let dir = CStr::from_ptr((API.assume_init().get_addon_directory)(convert_string("")))
-        .to_str()
-        .unwrap()
-        .to_string();
-    let wd = WalkDir::new(dir.clone());
-    let wd_it = wd.into_iter().filter_entry(|e| {
-        if e.file_type().is_dir() && e.file_name().to_str().unwrap().contains("common") {
-            return false;
-        }
-        return true;
-    });
-
-    let docs_dir = dirs_next::document_dir().unwrap();
-    let backup_dir = docs_dir.join("nexus-configs");
-    if !backup_dir.exists() {
-        fs::create_dir_all(&backup_dir).unwrap();
-    }
-    let local_time = chrono::Local::now();
-    let backup_file = match File::create(backup_dir.join(format!("backup-{}.zip", local_time.format("%Y-%m-%d-%H-%M")))) {
-        Ok(b) => b,
-        Err(err) => {
-            log(ELogLevel::CRITICAL, format!("Failed to create file {err}").to_string());
-            return;
-        }
-    };
-
-    let mut zip = zip::ZipWriter::new(backup_file);
-    let mut buffer = Vec::new();
-
-    let options = FileOptions::default();
-
-    for e in wd_it {
-        let entry = e.unwrap();
-        let path = entry.path();
-        let name = path.strip_prefix(dir.clone()).unwrap();
-
-        if path.is_file() {
-            if name.to_str().unwrap().to_string().contains(".dll") {
-                // log(ELogLevel::DEBUG, format!("Skipping {path:?}...").to_string());
-                continue;
+pub unsafe fn run_backup() -> bool {
+    let handler = thread::spawn(|| {
+        let dir = CStr::from_ptr((API.assume_init().get_addon_directory)(convert_string("")))
+            .to_str()
+            .unwrap()
+            .to_string();
+        let wd = WalkDir::new(dir.clone());
+        let wd_it = wd.into_iter().filter_entry(|e| {
+            if e.file_type().is_dir() && e.file_name().to_str().unwrap().contains("common") {
+                return false;
             }
-            #[allow(deprecated)]
-            zip.start_file_from_path(name, options).unwrap();
-            let mut f = File::open(path).unwrap();
+            return true;
+        });
 
-            f.read_to_end(&mut buffer).unwrap();
-            zip.write_all(&buffer).unwrap();
-            buffer.clear();
-
-            // log(ELogLevel::DEBUG, format!("Adding {path:?} as {name:?}...").to_string());
-        } else if !name.as_os_str().is_empty() {
-            // log(ELogLevel::DEBUG, format!("Adding dir {path:?} as {name:?}...").to_string());
-            #[allow(deprecated)]
-            zip.add_directory_from_path(name, options).unwrap();
+        let docs_dir = dirs_next::document_dir().unwrap();
+        let backup_dir = docs_dir.join("nexus-configs");
+        if !backup_dir.exists() {
+            fs::create_dir_all(&backup_dir).unwrap();
         }
-    }
-    zip.finish().unwrap();
+        let local_time = chrono::Local::now();
+        let backup_file = match File::create(backup_dir.join(format!("backup-{}.zip", local_time.format("%Y-%m-%d-%H-%M")))) {
+            Ok(b) => b,
+            Err(err) => {
+                log(ELogLevel::CRITICAL, format!("Failed to create file {err}").to_string());
+                return;
+            }
+        };
+
+        let mut zip = zip::ZipWriter::new(backup_file);
+        let mut buffer = Vec::new();
+
+        let options = FileOptions::default();
+
+        for e in wd_it {
+            let entry = e.unwrap();
+            let path = entry.path();
+            let name = path.strip_prefix(dir.clone()).unwrap();
+
+            if path.is_file() {
+                if name.to_str().unwrap().to_string().contains(".dll") {
+                    // log(ELogLevel::DEBUG, format!("Skipping {path:?}...").to_string());
+                    continue;
+                }
+                #[allow(deprecated)]
+                zip.start_file_from_path(name, options).unwrap();
+                let mut f = File::open(path).unwrap();
+
+                f.read_to_end(&mut buffer).unwrap();
+                zip.write_all(&buffer).unwrap();
+                buffer.clear();
+
+                // log(ELogLevel::DEBUG, format!("Adding {path:?} as {name:?}...").to_string());
+            } else if !name.as_os_str().is_empty() {
+                // log(ELogLevel::DEBUG, format!("Adding dir {path:?} as {name:?}...").to_string());
+                #[allow(deprecated)]
+                zip.add_directory_from_path(name, options).unwrap();
+            }
+        }
+        zip.finish().unwrap();
+    });
+    true
 }
 
 pub fn log(level: ELogLevel, s: String) {
@@ -128,15 +132,24 @@ pub unsafe extern "C" fn addon_shortcut() {
     let ui = UI.assume_init_ref();
 
     ui.separator();
-    ui.text_disabled("Config Backup");
     let clicked = ui.button("Run Backup");
     if clicked {
-        run_backup();
+        let finished = run_backup();
+        if finished {
+            log(ELogLevel::INFO, "Finished saving backup to nexus-configs folder".to_string());
+        }
     }
+}
+
+pub unsafe extern "C" fn render_options() {
+    let ui = UI.assume_init_ref();
+
+    ui.button("Test backup (Doesn't do anything)");
 }
 
 unsafe extern "C" fn unload() {
     (API.assume_init().remove_simple_shortcut)(convert_string(SHORTCUT_ID));
+    (API.assume_init().unregister_render)(render_options);
 }
 
 #[no_mangle]
